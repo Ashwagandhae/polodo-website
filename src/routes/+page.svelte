931187055438177145d03ae0720c8aeac20842b8
Ashwagandhae <julianlianbauer@gmail.com>
2023-12-05 12:12:03 -0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import './style.css';
+</script>
+
+<div class="one"></div>
+<div class="two"></div>
+<div class="three"></div>
+
+<div>
+  <img src="/logo.png" />
+  <a
+    href="https://form.jotform.com/233385613498062
+  ">Sign up here</a
+  >
+</div>
